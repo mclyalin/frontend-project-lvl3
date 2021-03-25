41f08994dd91f1ed8cdac8e389c8be500f8ad6c4
Michael Lyalin <mc.lyalin@gmail.com>
2021-03-25 16:49:10 +0500

    added loading rss data from url

--- src/init.js
+++ src/init.js
@@ -1,6 +1,30 @@
+import axios from 'axios';
 import * as yup from 'yup';
-// import axios from 'axios';
+import _ from 'lodash';
 import initView from './view.js';
+
+const parse = (str) => {
+  const doc = (new DOMParser()).parseFromString(str, 'text/xml');
+  if (doc.querySelector('parsererror')) {
+    return null;
+  }
+
+  const title = doc.querySelector('title').textContent;
+  const description = doc.querySelector('description').textContent;
+  const link = doc.querySelector('link').textContent;
+  const elements = doc.querySelectorAll('item');
+  const items = Array.from(elements).map((element) => ({
+    title: element.querySelector('title').textContent,
+    description: element.querySelector('description').textContent,
+    link: element.querySelector('link').textContent,
+  }));
+  return {
+    title,
+    description,
+    link,
+    items,
+  };
+};
 
 const validate = (value, links = []) => {
   const schema = yup
@@ -13,35 +37,34 @@ const validate = (value, links = []) => {
     schema.validateSync(value);
     return null;
   } catch (err) {
-    console.log(err.message);
-    return err.message;
+    return err.type;
   }
 };
 
 export default () => {
   const state = {
-    error: null,
+    feeds: [],
+    posts: [],
+    loadingProcess: {
+      status: 'idle',
+      error: null,
+    },
     form: {
-      status: 'filling',
-      fields: {
-        input: {
-          valid: true,
-          error: null,
-        },
-      },
+      error: null,
+      valid: true,
     },
   };
 
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('.rss-form input'),
-    button: document.querySelector('.rss-form button'),
+    button: document.querySelector('.rss-form button[type="submit"]'),
     feedback: document.querySelector('.feedback'),
     feeds: document.querySelector('.feeds'),
     posts: document.querySelector('.posts'),
   };
 
-  const watched = initView(state, elements);
+  const watchedState = initView(state, elements);
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -49,18 +72,74 @@ export default () => {
     const formData = new FormData(e.target);
     const value = formData.get('url').trim();
 
-    const error = validate(value);
-    if (error) {
-      watched.form.fields.input = {
-        error,
+    const validationError = validate(value);
+    if (validationError) {
+      watchedState.form = {
+        error: validationError,
         valid: false,
       };
       return;
     }
 
-    watched.form.fields.input = {
+    watchedState.form = {
       error: null,
       valid: true,
     };
+
+    const url = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
+    url.searchParams.set('url', value);
+    const promise = axios.get(url.toString());
+
+    watchedState.loadingProcess = {
+      status: 'loading',
+      error: null,
+    };
+
+    promise
+      .then((response) => {
+        const data = parse(response.data.contents);
+        if (!data) {
+          watchedState.loadingProcess = {
+            status: 'failed',
+            error: 'noRss',
+          };
+        }
+        const feed = {
+          id: _.uniqueId(),
+          url: data.link,
+          title: data.title,
+          description: data.description,
+        };
+
+        const posts = data.items.map((item) => ({
+          id: _.uniqueId(),
+          channelId: feed.id,
+          url: item.link,
+          title: item.title,
+          description: item.description,
+        }));
+
+        watchedState.feeds = [
+          feed,
+          ...watchedState.feeds,
+        ];
+
+        watchedState.posts = [
+          ...posts,
+          ...watchedState.posts,
+        ];
+
+        watchedState.loadingProcess = {
+          error: null,
+          state: 'idle',
+        };
+      })
+      .catch((err) => {
+        watchedState.loadingProcess = {
+          status: 'failed',
+          error: 'network',
+        };
+        throw err;
+      });
   });
 };
